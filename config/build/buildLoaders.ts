a29876bc5897e7032116a6c7cf60841f596fc172
BulatGim <bulat.gimaletdinov.01@gmail.com>
2022-09-25 14:30:32 +0300

    add babel, babel plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,27 @@ import {BuildEnv, BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const babelLoader = {
+        test: /\.(js|ts|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ["ru", "en"],
+                            keyAsDefaultValue: true
+                        }
+                    ]
+
+                ]
+            }
+        }
+    }
+
     const fileLoader = {
             test: /\.(png|jpe?g|gif|woff|woff2)$/i,
             use: [
@@ -44,6 +65,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgrLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader
     ]
