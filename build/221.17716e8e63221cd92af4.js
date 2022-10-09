444a6bca11ad8c2ed2b95d4c3781a34ecc5e1f31
BulatGim <bulat.gimaletdinov.01@gmail.com>
2022-10-09 19:28:24 +0300

    added screenshot testing for ci

---
+++ build/221.17716e8e63221cd92af4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkproduction_project=self.webpackChunkproduction_project||[]).push([[221],{221:(t,r,c)=>{c.r(r),c.d(r,{default:()=>u});var e=c(893),n=c(327);const u=function(){var t=(0,n.$)("about"),r=t.t;return t.i18n,(0,e.jsx)("div",{children:r("О сайте")})}}}]);
