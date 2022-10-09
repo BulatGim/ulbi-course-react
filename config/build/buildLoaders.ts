4e86fdc1d0eb70bf15afc13cadb73f98ce086428
BulatGim <bulat.gimaletdinov.01@gmail.com>
2022-10-09 18:54:46 +0300

    added tests for CI

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -39,7 +39,22 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     };
 
-    const cssLoader = buildCssLoader(isDev);
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]',
+                    },
+                },
+            },
+            'sass-loader',
+        ],
+    };
 
     const typescriptLoader = {
         test: /\.tsx?$/,
